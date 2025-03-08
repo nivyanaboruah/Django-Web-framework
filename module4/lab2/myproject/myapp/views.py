c5b3778e8adada8696eab23f051f52db70fba91a
nivyanaboruah <boruahnivyana@gmail.com>
2025-03-08 12:08:03 +0530

    Delete module4 directory

--- module4/lab2/myproject/myapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Menu
-
-# Create your views here.
-def menu(request):
-    menu_items = Menu.objects.all()
-    items_dict = {"menu": menu_items}
-    return render(request, "menu.html", items_dict)
